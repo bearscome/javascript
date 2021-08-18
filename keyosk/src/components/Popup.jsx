4d36413f62481189bbe7a976ac59c43ccdeacc19
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-08-19 02:21:47 +0900

    리얼 스테이트 바꾸는거 존나 힘드네

--- keyosk/src/components/Popup.jsx
+++ keyosk/src/components/Popup.jsx
@@ -7,26 +7,26 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
 
     const [mainItem, setMainItem] = React.useState(1);
     const [subMenuCount, setSubMenuCount] = React.useState({
-            beef: {
-                count: 0,
-                info: chageItemListData[0]
-            },
-            chicken: {
-                count: 0,
-                info: chageItemListData[1]
-            },
-            egg: {
-                count: 0,
-                info: chageItemListData[2]
-            },
-            lettuce: {
-                count: 0,
-                info: chageItemListData[3]
-            },
-            buns: {
-                count: 0,
-                info: chageItemListData[4]
-            }
+        beef: {
+            count: 0,
+            info: chageItemListData[0]
+        },
+        chicken: {
+            count: 0,
+            info: chageItemListData[1]
+        },
+        egg: {
+            count: 0,
+            info: chageItemListData[2]
+        },
+        lettuce: {
+            count: 0,
+            info: chageItemListData[3]
+        },
+        buns: {
+            count: 0,
+            info: chageItemListData[4]
+        }
     })
     const [clickChange, setClickChange] = React.useState(false);
     
@@ -37,55 +37,61 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
     const plus = (el) => {
         switch(el.name) {
             case '소고기 패티' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount:{
-                        beef: {
-                            count: subMenuCount.beef.count += 1,
-                        },
+                setSubMenuCount((state) =>({
+                    ...state,
+                    beef: {
+                        count: state.beef.count + 1,
+                        info:chageItemListData[0]
                     }
-                });
-                console.log('subMenuCount123123123', subMenuCount.beef.count);
+                }));
                 break;
+                // setSubMenuCount({
+                //     ...subMenuCount,
+                //     subMenuCount: {
+                //         beef: {
+                //             count : subMenuCount.chicken.count += 1
+                //         }
+                //     }
+                // });
+                /**
+                 * () => ()이건 무슨 뜻 일까? 즉시 실행 함수인가?
+                 * setState할 때, 함수를 선언하는 것과 객체를 넣는것의 차이는 무엇인가?
+                 */
             case '치킨 패티' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        chicken: {
-                            count : subMenuCount.chicken.count += 1
-                        }
+                setSubMenuCount((state) =>({
+                    ...state,
+                    chicken: {
+                        count: state.chicken.count + 1,
+                        info:chageItemListData[1]
                     }
-                });
+                }));
                 break;
             case '계란' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        egg: {
-                            count: subMenuCount.egg.count += 1
-                        }
+                setSubMenuCount((state) =>({
+                    ...state,
+                    egg: {
+                        count: state.egg.count + 1,
+                        info:chageItemListData[2]
                     }
-                });
+                }));
                 break;
             case '양상추' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        lettuce: {
-                            count: subMenuCount.lettuce.count += 1
-                        }
+                setSubMenuCount((state) =>({
+                    ...state,
+                    lettuce: {
+                        count: state.lettuce.count + 1,
+                        info:chageItemListData[3]
                     }
-                });
+                }));
                 break;
             case '햄버거 빵' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        buns: {
-                            count: subMenuCount.buns.count += 1
-                        }
+                setSubMenuCount((state) =>({
+                    ...state,
+                    buns: {
+                        count: state.buns.count + 1,
+                        info:chageItemListData[4]
                     }
-                });
+                }));
                 break;
         }
     }
@@ -93,54 +99,49 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
     const minus = (el) => {
         switch(el.name) {
             case '소고기 패티' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        beef: {
-                            count : subMenuCount.beef.count < 1 ? 0 : subMenuCount.beef.count -= 1,
-                        }
+                setSubMenuCount((state) => ({
+                    ...state,
+                    beef: {
+                        count: state.beef.count < 1 ? 0 : state.beef.count - 1,
+                        info:chageItemListData[0]
                     }
-                });
+                }));
                 break;
             case '치킨 패티' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        chicken: {
-                            count :subMenuCount.chicken.count < 1 ? 0 : subMenuCount.chicken.count -= 1,
-                        }
+                setSubMenuCount((state) => ({
+                    ...state,
+                    chicken: {
+                        count: state.chicken.count < 1 ? 0 : state.chicken.count - 1,
+                        info:chageItemListData[0]
                     }
-                });
+                }));
                 break;
             case '계란' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        egg: {
-                            count : subMenuCount.egg.count < 1 ? 0 : subMenuCount.egg.count -= 1,
-                        }
+                setSubMenuCount((state) => ({
+                    ...state,
+                    egg: {
+                        count: state.egg.count < 1 ? 0 : state.egg.count - 1,
+                        info:chageItemListData[0]
                     }
-                });
+                }));
                 break;
             case '양상추' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        lettuce: {
-                            count : subMenuCount.lettuce.count < 1 ? 0 : subMenuCount.lettuce.count -= 1,
-                        }
+                setSubMenuCount((state) => ({
+                    ...state,
+                    lettuce: {
+                        count: state.lettuce.count < 1 ? 0 : state.lettuce.count - 1,
+                        info:chageItemListData[0]
                     }
-                });
+                }));
                 break;
             case '햄버거 빵' :
-                setSubMenuCount({
-                    ...subMenuCount,
-                    subMenuCount: {
-                        buns: {
-                            count : subMenuCount.buns.count < 1 ? 0 : subMenuCount.buns.count -= 1,
-                        }
+                setSubMenuCount((state) => ({
+                    ...state,
+                    buns: {
+                        count: state.buns.count < 1 ? 0 : state.buns.count - 1,
+                        info:chageItemListData[0]
                     }
-                });
+                }));
                 break;
         }
     }
@@ -190,27 +191,20 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
         alert('변경할 메뉴를 어드민에서 추가해주세요.')
     }
 
+    if(sendItem?.addItem) {
+        console.log('sendItem?.addItem', sendItem?.addItem)
+        showSubItem = Object.entries(sendItem?.addItem).map((el, idx) => {
+            return (
+                <li>
+                    <p>고르신 메뉴: {el && el[1]?.info?.name}</p>
+                    <p>고르신 개수: {el && el[1]?.count}</p>
+                    <p>가격: {el && el[1]?.info?.price}</p>
+                </li>
+            )
+        }) 
+    }
+    
 
-    // const testaa = sendItem?.addItem?.map((el, idx) => {
-    //     // let name = Object.keys(sendItem?.addItem[idx]);
-    //     //key가 다 달라서 el로 접근이 불가하다.  
-    //     //Object.keys로 인덱스 값으로 접근하여 변수에 할당한다.
-
-    //     // console.log('aaaaaa',name)
-    //     // console.log(idx + 1);
-    //     // console.log(el);
-    //     // console.log(el[name]);
-    //     // console.log(el[name]?.count)
-
-    //     // return (
-    //     //     <li style = {el[name]?.count > 0 ? {display:'block'} : {display:'none'}}>
-    //     //         <p>고르신 메뉴: {el[name]?.count > 0 ? el[name]?.info?.name : ''}</p>
-    //     //         <p>고르신 개수: {el[name]?.count > 0 ? el[name]?.info?.count : ''}</p>
-    //     //         <p>가격: {el[name]?.count > 0 ? el[name]?.info?.price : ''}</p>
-    //     //     </li>
-    //     // )
-        
-    // })
     return (
         <React.Fragment>
             <div className="popup" style={clicked ? {display:'block'} : {display:'none'}}>
@@ -253,32 +247,7 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
                 <section style={!clickChange ? {display:'block'} : {display:'none'}}>
                     <p>추가된 메뉴</p>
                     <ul>
-                        {/* {testaa} */}
-                        {/* <li style = {sendItem?.addItem[0]?.beef?.count > 0 ? {display:'block'} : {display:'none'}}>
-                            <p>고르신 메뉴: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.info?.name : ''}</p>
-                            <p>갯수: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.count : ''}</p>
-                            <p>가격: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.info?.price : ''}</p>
-                        </li>
-                        <li style = {sendItem?.addItem[1]?.chicken?.count > 0  ? {display:'block'} : {display:'none'}}>
-                            <p>고르신 메뉴: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.info?.name : ''}</p>
-                            <p>갯수: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.count : ''}</p>
-                            <p>가격: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.info?.price : ''}</p>
-                        </li>
-                        <li style = {sendItem?.addItem[2]?.egg?.count > 0  ? {display:'block'} : {display:'none'}}>
-                            <p>고르신 메뉴: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.info?.name : ''}</p>
-                            <p>갯수: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.count : ''}</p>
-                            <p>가격: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.info?.price : ''}</p>
-                        </li>
-                        <li style = {sendItem?.addItem[3]?.lettuce?.count > 0  ? {display:'block'} : {display:'none'}}>
-                            <p>고르신 메뉴: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[0]?.lettuce?.info?.name : ''}</p>
-                            <p>갯수: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[3]?.lettuce?.count : ''}</p>
-                            <p>가격: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[3]?.lettuce?.info?.price : ''}</p>
-                        </li>
-                        <li style = {sendItem?.addItem[4]?.buns?.count > 0  ? {display:'block'} : {display:'none'}}>
-                            <p>고르신 메뉴: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.info?.name : ''}</p>
-                            <p>갯수: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.count : ''}</p>
-                            <p>가격: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.info?.price : ''}</p>
-                        </li> */}
+                        {showSubItem}
                     </ul>
                     <button className="completeFunc">변경 완료</button>
                 </section>
