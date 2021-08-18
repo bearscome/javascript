4c31db8134a9f9de3bc0a7dd95db2b67ebd1eb9b
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2021-08-19 01:00:44 +0900

    팝업 구조 변경

--- keyosk/src/components/Popup.jsx
+++ keyosk/src/components/Popup.jsx
@@ -7,76 +7,83 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
 
     const [mainItem, setMainItem] = React.useState(1);
     const [subMenuCount, setSubMenuCount] = React.useState({
-        beef: {
-            count: 0,
-            info: chageItemListData[0]
-        },
-        chicken: {
-            count: 0,
-            info: chageItemListData[1]
-        },
-        egg: {
-            count: 0,
-            info: chageItemListData[2]
-        },
-        lettuce: {
-            count: 0,
-            info: chageItemListData[3]
-        },
-        buns: {
-            count: 0,
-            info: chageItemListData[4]
-        }
+            beef: {
+                count: 0,
+                info: chageItemListData[0]
+            },
+            chicken: {
+                count: 0,
+                info: chageItemListData[1]
+            },
+            egg: {
+                count: 0,
+                info: chageItemListData[2]
+            },
+            lettuce: {
+                count: 0,
+                info: chageItemListData[3]
+            },
+            buns: {
+                count: 0,
+                info: chageItemListData[4]
+            }
     })
     const [clickChange, setClickChange] = React.useState(false);
     
     let chageItemList = null;
     let sendItem = null;
+    let showSubItem = null;
 
     const plus = (el) => {
         switch(el.name) {
             case '소고기 패티' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    beef: {
-                        count : subMenuCount.beef.count + 1,
-                        info: chageItemListData[0]
+                    subMenuCount:{
+                        beef: {
+                            count: subMenuCount.beef.count += 1,
+                        },
                     }
                 });
+                console.log('subMenuCount123123123', subMenuCount.beef.count);
                 break;
             case '치킨 패티' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    chicken: {
-                        count : subMenuCount.chicken.count + 1,
-                        info: chageItemListData[1]
+                    subMenuCount: {
+                        chicken: {
+                            count : subMenuCount.chicken.count += 1
+                        }
                     }
                 });
                 break;
             case '계란' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    egg: {
-                        count : subMenuCount.egg.count + 1,
-                        info: chageItemListData[2]
+                    subMenuCount: {
+                        egg: {
+                            count: subMenuCount.egg.count += 1
+                        }
                     }
                 });
                 break;
             case '양상추' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    lettuce: {
-                        count : subMenuCount.lettuce.count + 1,
-                        info: chageItemListData[3]
+                    subMenuCount: {
+                        lettuce: {
+                            count: subMenuCount.lettuce.count += 1
+                        }
                     }
                 });
                 break;
             case '햄버거 빵' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    buns: {
-                        count : subMenuCount.buns.count + 1,
-                        info: chageItemListData[4]
+                    subMenuCount: {
+                        buns: {
+                            count: subMenuCount.buns.count += 1
+                        }
                     }
                 });
                 break;
@@ -88,45 +95,50 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
             case '소고기 패티' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    beef: {
-                        count : subMenuCount.beef.count < 1 ? 0 : subMenuCount.beef.count - 1,
-                        info: chageItemListData[0]
+                    subMenuCount: {
+                        beef: {
+                            count : subMenuCount.beef.count < 1 ? 0 : subMenuCount.beef.count -= 1,
+                        }
                     }
                 });
                 break;
             case '치킨 패티' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    chicken: {
-                        count : subMenuCount.chicken.count < 1 ? 0 : subMenuCount.chicken.count - 1,
-                        info: chageItemListData[1]
+                    subMenuCount: {
+                        chicken: {
+                            count :subMenuCount.chicken.count < 1 ? 0 : subMenuCount.chicken.count -= 1,
+                        }
                     }
                 });
                 break;
             case '계란' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    egg: {
-                        count : subMenuCount.egg.count < 1 ? 0 : subMenuCount.egg.count - 1,
-                        info: chageItemListData[2]
+                    subMenuCount: {
+                        egg: {
+                            count : subMenuCount.egg.count < 1 ? 0 : subMenuCount.egg.count -= 1,
+                        }
                     }
                 });
                 break;
             case '양상추' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    lettuce : {
-                        count : subMenuCount.lettuce.count < 1 ? 0 : subMenuCount.lettuce.count - 1,
-                        info: chageItemListData[3]
+                    subMenuCount: {
+                        lettuce: {
+                            count : subMenuCount.lettuce.count < 1 ? 0 : subMenuCount.lettuce.count -= 1,
+                        }
                     }
                 });
                 break;
             case '햄버거 빵' :
                 setSubMenuCount({
                     ...subMenuCount,
-                    buns : {
-                        count : subMenuCount.buns.count < 1 ? 0 : subMenuCount.buns.count - 1,
-                        info: chageItemListData[4]
+                    subMenuCount: {
+                        buns: {
+                            count : subMenuCount.buns.count < 1 ? 0 : subMenuCount.buns.count -= 1,
+                        }
                     }
                 });
                 break;
@@ -138,19 +150,9 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
         setClickChange(!clickChange)
     }
 
-    const showChangeItem = () => {
-
-        return (
-            <React.Fragment>
-                <p></p>
-            </React.Fragment>
-        )
-    }
-
     if(chageItemListData.length > 0) {
         
         chageItemList = chageItemListData.map((el, idx) => {
-
             return(
                 <li className="changeItem" key= {idx}>
                     <div className="itemWrap">
@@ -163,12 +165,13 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
                     <div className="itemfunc">
                         <button onClick = {()=> {
                             minus(el);
+                            console.log('elel', el);
                         }}>-</button>
-                        <p style = {el.name == "소고기 패티" ? {display:'block'} : {display:'none'}}>{el.name === "소고기 패티" && subMenuCount.beef.count}</p>
-                        <p style = {el.name == "치킨 패티" ? {display:'block'} : {display:'none'}}>{el.name === "치킨 패티" && subMenuCount.chicken.count}</p>
-                        <p style = {el.name == "계란" ? {display:'block'} : {display:'none'}}>{el.name === "계란" && subMenuCount.egg.count}</p>
-                        <p style = {el.name == "양상추" ? {display:'block'} : {display:'none'}}>{el.name === "양상추" && subMenuCount.lettuce.count}</p>
-                        <p style = {el.name == "햄버거 빵" ? {display:'block'} : {display:'none'}}>{el.name === "햄버거 빵" && subMenuCount.buns.count}</p>
+                            <p style = {el.name == "소고기 패티" ? {display:'block'} : {display:'none'}}>{el.name === "소고기 패티" && subMenuCount?.beef?.count}</p>
+                            <p style = {el.name == "치킨 패티" ? {display:'block'} : {display:'none'}}>{el.name === "치킨 패티" && subMenuCount?.chicken?.count}</p>
+                            <p style = {el.name == "계란" ? {display:'block'} : {display:'none'}}>{el.name === "계란" && subMenuCount?.egg?.count}</p>
+                            <p style = {el.name == "양상추" ? {display:'block'} : {display:'none'}}>{el.name === "양상추" && subMenuCount?.lettuce?.count}</p>
+                            <p style = {el.name == "햄버거 빵" ? {display:'block'} : {display:'none'}}>{el.name === "햄버거 빵" && subMenuCount?.buns?.count}</p>
                         <button onClick = {() => {
                             plus(el);
                         }}>+</button>
@@ -183,15 +186,31 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
         }
         console.log('selectedItem', selectedItem)
         console.log('sendItem' ,sendItem)
-        
     } else {
         alert('변경할 메뉴를 어드민에서 추가해주세요.')
     }
 
 
+    // const testaa = sendItem?.addItem?.map((el, idx) => {
+    //     // let name = Object.keys(sendItem?.addItem[idx]);
+    //     //key가 다 달라서 el로 접근이 불가하다.  
+    //     //Object.keys로 인덱스 값으로 접근하여 변수에 할당한다.
 
-   
+    //     // console.log('aaaaaa',name)
+    //     // console.log(idx + 1);
+    //     // console.log(el);
+    //     // console.log(el[name]);
+    //     // console.log(el[name]?.count)
 
+    //     // return (
+    //     //     <li style = {el[name]?.count > 0 ? {display:'block'} : {display:'none'}}>
+    //     //         <p>고르신 메뉴: {el[name]?.count > 0 ? el[name]?.info?.name : ''}</p>
+    //     //         <p>고르신 개수: {el[name]?.count > 0 ? el[name]?.info?.count : ''}</p>
+    //     //         <p>가격: {el[name]?.count > 0 ? el[name]?.info?.price : ''}</p>
+    //     //     </li>
+    //     // )
+        
+    // })
     return (
         <React.Fragment>
             <div className="popup" style={clicked ? {display:'block'} : {display:'none'}}>
@@ -233,6 +252,34 @@ const Popup = ({selectedItem, clicked, chageItemListData, returnItem}) => {
                 </section>
                 <section style={!clickChange ? {display:'block'} : {display:'none'}}>
                     <p>추가된 메뉴</p>
+                    <ul>
+                        {/* {testaa} */}
+                        {/* <li style = {sendItem?.addItem[0]?.beef?.count > 0 ? {display:'block'} : {display:'none'}}>
+                            <p>고르신 메뉴: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.info?.name : ''}</p>
+                            <p>갯수: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.count : ''}</p>
+                            <p>가격: {sendItem?.addItem[0]?.beef?.count > 0 ? sendItem?.addItem[0]?.beef?.info?.price : ''}</p>
+                        </li>
+                        <li style = {sendItem?.addItem[1]?.chicken?.count > 0  ? {display:'block'} : {display:'none'}}>
+                            <p>고르신 메뉴: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.info?.name : ''}</p>
+                            <p>갯수: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.count : ''}</p>
+                            <p>가격: {sendItem?.addItem[1]?.chicken?.count > 0 ? sendItem?.addItem[1]?.chicken?.info?.price : ''}</p>
+                        </li>
+                        <li style = {sendItem?.addItem[2]?.egg?.count > 0  ? {display:'block'} : {display:'none'}}>
+                            <p>고르신 메뉴: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.info?.name : ''}</p>
+                            <p>갯수: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.count : ''}</p>
+                            <p>가격: {sendItem?.addItem[2]?.egg?.count > 0 ? sendItem?.addItem[2]?.egg?.info?.price : ''}</p>
+                        </li>
+                        <li style = {sendItem?.addItem[3]?.lettuce?.count > 0  ? {display:'block'} : {display:'none'}}>
+                            <p>고르신 메뉴: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[0]?.lettuce?.info?.name : ''}</p>
+                            <p>갯수: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[3]?.lettuce?.count : ''}</p>
+                            <p>가격: {sendItem?.addItem[3]?.lettuce?.count > 0 ? sendItem?.addItem[3]?.lettuce?.info?.price : ''}</p>
+                        </li>
+                        <li style = {sendItem?.addItem[4]?.buns?.count > 0  ? {display:'block'} : {display:'none'}}>
+                            <p>고르신 메뉴: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.info?.name : ''}</p>
+                            <p>갯수: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.count : ''}</p>
+                            <p>가격: {sendItem?.addItem[4]?.buns?.count > 0 ? sendItem?.addItem[4]?.buns?.info?.price : ''}</p>
+                        </li> */}
+                    </ul>
                     <button className="completeFunc">변경 완료</button>
                 </section>
             </div>
